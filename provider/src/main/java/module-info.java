e6b4052fc704df6c37bc5b76a3972b1e8102112f
cybertobbe <143131053+cybertobbe@users.noreply.github.com>
2024-04-18 15:42:28 +0200

    Add dockerfile

--- provider/src/main/java/module-info.java
+++ provider/src/main/java/module-info.java
@@ -2,6 +2,7 @@ import com.example.consumer.provider.Quotes;
 import com.example.consumer.service.Converter;
 
 module provider {
+    exports com.example.consumer.provider;
     provides Converter with Quotes;
     requires service;
 
