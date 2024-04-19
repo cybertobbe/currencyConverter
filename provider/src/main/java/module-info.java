d6df96ccdfe9d0122d446d5b1d2c421d5a9d2713
cybertobbe <143131053+cybertobbe@users.noreply.github.com>
2024-04-19 16:13:05 +0200

    Add eurusd provider

--- provider/src/main/java/module-info.java
+++ provider/src/main/java/module-info.java
@@ -1,11 +1,12 @@
 import com.example.consumer.provider.eursek;
+import com.example.consumer.provider.eurusd;
 import com.example.consumer.provider.gbpsek;
 import com.example.consumer.provider.usdsek;
 import com.example.consumer.service.Converter;
 
 module provider {
 
-    provides Converter with eursek, usdsek, gbpsek;
+    provides Converter with eursek, usdsek, gbpsek, eurusd;
     requires service;
 
 }
