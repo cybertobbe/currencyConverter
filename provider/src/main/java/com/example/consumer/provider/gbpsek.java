77e3f8aed7a5c920601f687fffba043589d5dfe9
cybertobbe <143131053+cybertobbe@users.noreply.github.com>
2024-04-19 16:16:22 +0200

    Add eurusd provider

--- provider/src/main/java/com/example/consumer/provider/gbpsek.java
+++ provider/src/main/java/com/example/consumer/provider/gbpsek.java
@@ -11,7 +11,6 @@ public class gbpsek implements Converter {
     @Override
     public BigDecimal convert(double value) {
         double rate = 13.60;
-        String counterCurrency = "SEK";
         BigDecimal bd = new BigDecimal(Double.toString(value*rate));
         bd = bd.setScale(2, RoundingMode.HALF_UP);
         return bd;
